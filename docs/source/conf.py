a8ac14e69ccc8dabe15b4e48e256ea309a577a09
Derek Weitzel <djw8605@gmail.com>
2014-04-03 12:09:01 -0500

    Bumping version to 0.5.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ copyright = u'2014, Derek Weitzel'
 # built documents.
 #
 # The short X.Y version.
-version = '0.4'
+version = '0.5'
 # The full version, including alpha/beta/rc tags.
 release = '1'
 
